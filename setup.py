8ebdb30391f273d06ac63201432a29c6d63a3355
Thomas Jost <schnouki@schnouki.net>
2012-05-30 22:40:18 +0200

    Initial config file creation
    If the user doesn't have a config file, interactively create one using the
    sample one and asking for JID and password.

--- setup.py
+++ setup.py
@@ -25,6 +25,9 @@ setup(name="bccc",
 
       packages=find_packages(),
       scripts=["bin/bccc"],
+      package_data={
+          "bccc": ["bccc.conf.sample"]
+      },
       include_package_data=True,
 
       setup_requires=["distribute"],
