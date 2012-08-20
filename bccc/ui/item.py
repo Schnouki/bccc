27ce81b7ad42f0044e3802ac6ac385dd8a85eaba
Thomas Jost <schnouki@schnouki.net>
2012-08-21 01:42:44 +0200

    Minor cleanup

--- bccc/ui/item.py
+++ bccc/ui/item.py
@@ -11,8 +11,6 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-import bisect
-
 import dateutil.tz
 import urwid
 
